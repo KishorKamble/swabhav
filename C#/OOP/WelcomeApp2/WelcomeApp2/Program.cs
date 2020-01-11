93fcca121bb8cd6b26f4b22232a2dea615740eaa
unknown <Administrator@NGKW-LP-901.newgenkw.local>
2020-01-11 13:16:38 +0530

    WelcomeApp2 proj

---
+++ C#/OOP/WelcomeApp2/WelcomeApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace WelcomeApp2
+{
+    class Program
+    {
+        static void Main(string[] arguments)
+        {
+
+            Console.WriteLine(arguments.Length);
+
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine("Hello -" + args[i].ToString());
+            //}
+
+            foreach(string argument in arguments)
+            {
+                Console.WriteLine("Hello -" + argument );
+
+            }
+
+            Console.Read();   
+        }
+    }
+}
