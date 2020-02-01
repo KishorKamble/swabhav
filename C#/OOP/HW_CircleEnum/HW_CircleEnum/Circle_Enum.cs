82c22505e4b0a594ac95ba38f0b8efa02bdbe7a6
unknown <Administrator@NGKW-LP-901.newgenkw.local>
2020-02-01 12:23:10 +0530

    circle enum hw

---
+++ C#/OOP/HW_CircleEnum/HW_CircleEnum/Circle_Enum.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CircleEnum
+{
+    class Circle_Enum
+    {
+        private float _radius;
+        private float _area;
+        private float _perimeter;
+        private BorderColor _color;
+
+        public float radius
+        {
+
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+
+        public float CalculateArea
+        {
+
+            get
+            {
+                return (float)Math.Round(3.14f * _radius * _radius, 2, MidpointRounding.AwayFromZero);
+            }
+            set
+            {
+                _area = value;
+            }
+        }
+
+        public float Perimeter
+        {
+
+            get
+            {
+                return (float)Math.Round(2 * 3.14f * _radius, 2,
+               MidpointRounding.AwayFromZero);
+            }
+            set
+            {
+                _perimeter = value;
+            }
+        }
+
+        public BorderColor color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }      
+    }
+}
---
+++ C#/OOP/HW_CircleEnum/HW_CircleEnum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CircleEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Circle_Enum obj = new Circle_Enum();
+            obj.radius = 2.5f;
+            obj.color = BorderColor.RED;
+
+            Console.WriteLine("Radius - " + obj.radius);
+            Console.WriteLine("Circle Area - " + obj.CalculateArea);
+            Console.WriteLine("Perimeter - " + obj.Perimeter);
+            Console.WriteLine("Color Style - " + obj.color);
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+    }
+}
